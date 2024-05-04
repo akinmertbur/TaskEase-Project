e385daa2dcb28915785d2970157281e80c4ad437
Akin Mert Bur <akinmertbur@gmail.com>
2024-05-04 21:19:39 +0300

    establish database connection

--- index.js
+++ index.js
@@ -2,19 +2,29 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: process.env.user,
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port: process.env.port,
+});
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+let items = [];
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+  items = result.rows;
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
