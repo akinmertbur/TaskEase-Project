db976093445321b348e571f5d361f9469e4a9abd
Akin Mert Bur <akinmertbur@gmail.com>
2024-05-04 20:30:03 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
